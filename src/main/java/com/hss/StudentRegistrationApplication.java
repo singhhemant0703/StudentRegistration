9e7590f6e84070a77834316a7790684efb88c77b
Hemant singh <Hemant singh@DESKTOP-4NJF0F6>
2020-07-24 22:40:01 +0530

    commit first

---
+++ src/main/java/com/hss/StudentRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.hss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRegistrationApplication.class, args);
+	}
+
+}
